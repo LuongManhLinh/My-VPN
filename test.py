7ce9488c95ca36e6edcc1b4464f601c66aafd441
LuongManhLinh <luongmanhlinh2004cb@gmail.com>
2025-05-06 16:27:32 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, request, send_file, abort
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, world"
+@app.route("/file")
+def download_file():
+    key = request.args.get('key')
+    idx = request.args.get('idx')
+    if key == os.getenv("SECRET_KEY"): 
+        file_path = f"/etc/wireguard/clients/client{idx}.conf"  
+        try:
+            return send_file(file_path, as_attachment=True)
+        except FileNotFoundError:
+            abort(404, description="File not found")
+    else:
+        abort(403, description="Forbidden: Invalid key")
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000, debug=True)
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
